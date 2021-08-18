f39f0c7231462203471eca4cb7091d5363089b88
Lucas Campos <jlcamposj@falabella.cl>
2021-08-18 11:35:22 -0400

    removed offerControllers at controllers index base file

--- .cygnus-project/cygnus-base/templates/project/contrtollers/index.ts
+++ .cygnus-project/cygnus-base/templates/project/contrtollers/index.ts
@@ -1,5 +1,3 @@
 import { HealthController } from './health/health.controller'
-import { OfferController as OfferControllerV1 } from './offer/v1/offer.controller'
-import { OfferController as OfferControllerV2 } from './offer/v2/offer.controller'
 
-export const controllers = [HealthController, OfferControllerV1, OfferControllerV2]
+export const controllers = [HealthController]
